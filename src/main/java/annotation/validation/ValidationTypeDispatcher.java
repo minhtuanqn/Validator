83230a0f1eaaad48f8631191e8624b78ff88c7ea
minhtuanqn320@gmail.com <minhtuanqn320@gmail.com>
2021-05-04 15:44:03 +0700

    delete comment

--- src/main/java/annotation/validation/ValidationTypeDispatcher.java
+++ src/main/java/annotation/validation/ValidationTypeDispatcher.java
@@ -9,7 +9,6 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
